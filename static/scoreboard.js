117e0126db6cc449d43526f985879f60f0e934d7
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-06 19:51:42 -0400

    JS fills in crude scoreboard, no poll

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
 
     var update = function(data) {
         var players = data['sb'];
+        var $scores = $('#scores');
         for (var i = 0; i < players.length; i++) {
             var player = players[i];
-            console.log("id", player.pid);
-            console.log("name", player.player_name);
-            console.log("results", player.results);
+            var name = player.player_name;
+            $scores.append('<div class="row" id=' + player.pid + '></div>');
+            $row = $('#' + player.pid);
+            $row.append('<div class="box name">' + name + '</div>');
+            var results = player.results;
+            for (var j = 0; j < results.length; j++) {
+                var resultClass = results[j] === 1 ? "box result correct" : "box result";
+                $row.append('<div class="' + resultClass + '"></div>');
+            }
         }
     };
 
