f841b98ea79c3e4111b8d08ac5ba76e6bd0457be
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-06 05:13:23 -0400

    in progress

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
+    var update = function(data) {
+        var players = Object.keys(data);
+        for (var i = 0; i < players.length; i++) {
+            console.log(players[i]);
+        }
+    };
+
     $.ajax({
         url: '/score',
         type: 'GET',
         dataType: 'json',
-        success: function() {
-            alert("success!")
-        },
-        error: function() {
-            alert("error");
-        }
+        success: update
     });
 
 });
