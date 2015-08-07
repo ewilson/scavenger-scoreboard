94805890f38ab71e24288169e60c8d27bbc66e91
Eric Wilson <wilson.eric.n@gmail.com>
2015-08-06 20:19:37 -0400

    working, no polling

--- app.py
+++ app.py
@@ -9,22 +9,36 @@ Bower(app)
 def index():
     return render_template('index.html')
 
-sb_data = {'sb': [
-    {'pid': 1, 'player_name': 'Dalton & Riley',
-     'results': [1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]},
-    {'pid': 2, 'player_name': 'Grant & Daniel',
-     'results': [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0]}
-    ]}
+sb_data = {'sb': []}
 
 
+#
+# To Post a score send {"pid": 0, "results": [ ... ]}
+#
 @app.route('/score', methods=['GET', 'POST'])
 def score():
     if request.method == 'POST':
         update = request.get_json()
         global sb_data
-        sb_data[update['name']] = update['results']
+        players = sb_data['sb']
+        for i, player in enumerate(players):
+            if player['pid'] == update['pid']:
+                players[i]['results'] = update['results']
     return jsonify(**sb_data)
 
+
+#
+# To Post a new player, send {"player_name": "My Name"}
+#
+@app.route('/player', methods=['POST'])
+def player():
+    data = request.get_json()
+    global sb_data
+    players = sb_data['sb']
+    pid = len(players)
+    new_player = {'pid': pid, 'player_name': data['player_name'], 'results': [0]*15}
+    players.append(new_player)
+    return jsonify(**new_player)
 
 if __name__ == '__main__':
     app.run(debug=True)
